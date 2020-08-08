60222b8f2e25b983c88a32c3934fda9aa3dc0e89
Nateyana <nblake707@gmail.com>
2020-08-08 16:06:11 -0400

    styling nav component

---
+++ recipe/src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SideBar.css';
+
+const SideBar = () => {
+
+    return (
+        <nav>
+            <a href="#">Shop</a>
+            <a href="#">About</a>
+        </nav>
+    )
+
+}
+
+export default SideBar;
