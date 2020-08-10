6781252db72edc01a55efeb137b57904a801b8c4
Nateyana <nblake707@gmail.com>
2020-08-09 22:56:18 -0400

    Converted main component and worked out toggle feature

--- recipe/src/components/Main.js
+++ recipe/src/components/Main.js
@@ -1,18 +1,44 @@
 import React from "react";
+import './Main.css';
 import Menu from '../images/menu.svg';
+import Close from '../images/close.svg';
 
-const Main = () => {
-  return (
-    <main>
-      <a href="app" class="toggle-nav">
-        <img 
-          src={Menu} 
-          alt="menu icon" 
-        />
-        Menu
-      </a>
-    </main>
-  );
+class Main extends React.Component {
+
+    state = {
+        showMenu : false
+    }
+
+    toggleTag = (e) => {
+        // add the open class to the classList
+        e.preventDefault();
+        this.setState({
+            showMenu: !this.state.showMenu
+        })
+
+        console.log(this.state.showMenu);
+        
+    }
+
+render (){
+    const menuVis = this.state.showMenu ? 'open' : '';
+    const imageSrc = this.state.mainClass == "open" ? Close : Menu;
+
+
+
+    return (
+        <main className={menuVis}>
+          <a href="app" className="toggle-nav" onClick={this.toggleTag}>
+    
+            <img 
+              src={imageSrc} 
+              alt="menu icon" 
+            />
+            {/* {this.state.mainClass ==  "open" ? "Close" : "Menu"} */}
+          </a>
+        </main>
+      );
+}
 };
 
 export default Main;
